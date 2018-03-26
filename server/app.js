ffa9cd5a232fc6ac74d9d10b1d8cce3e020566dd
John <loydall@gmail.com>
2018-03-26 17:14:40 +0100

    Added basic reset, updated grid styling

--- server/app.js
+++ server/app.js
@@ -1,6 +1,4 @@
 const express = require('express')
 const app = express()
-
 app.use(express.static('dist'))
-
-app.listen(3000, () => console.log('Example app listening on port 3000!'))
+app.listen(3000, () => console.log('Listening on port 3000...'))
